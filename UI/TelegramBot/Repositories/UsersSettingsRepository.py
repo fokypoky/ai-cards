5eb07953265bac5f43bdd0d6c2360d8dc5dd36be
Anton Petukhov <petuhova805@gmail.com>
2024-01-26 00:55:26 +0300

    Метод обновления настроек

--- UI/TelegramBot/Repositories/UsersSettingsRepository.py
+++ UI/TelegramBot/Repositories/UsersSettingsRepository.py
@@ -18,3 +18,12 @@ class UsersSettingsRepository:
 
         print(response.text, response.status_code)
         return None, response.status_code 
+    
+    def update(self, user_settings: UserSettings) -> bool:
+        response = requests.put(f'http://{self.host}:{self.port}/api/user_settings/{user_settings.user_id}?game_type={user_settings.game_type}&default_bet={user_settings.default_bet}')
+        
+        if response.status_code == 200:
+            return True, 200
+        
+        print(response.text)
+        return False, response.status_code
